70010d77206227130369163af4cfec68b2d6d619
Jamal <Fox.Jamal@outlook.com>
2021-11-20 12:22:04 -0500

    linked animation library, spring. Added animations to NavBar & Hero section

--- src/Components/HERO/Hero.js
+++ src/Components/HERO/Hero.js
@@ -1,118 +1,148 @@
 import React from "react";
 import Particles from "react-tsparticles";
+import { useSpring, animated } from "react-spring";
 import "./Hero.css";
 
 const Hero = () => {
-  const particlesInit = (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  const backgroundAnimation = useSpring({
+    to: { opacity: 0.7 },
+    from: { opacity: 1 },
+    config: { duration: 2500 },
+    delay: 5000,
+  });
+  const hiFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 500 },
+    delay: 1500,
+  });
+  const imFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+    delay: 3500,
+  });
+  const nameFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+    delay: 4250,
+  });
+  const particleFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+    delay: 4250,
+  });
+  const taglineFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 2500 },
+    delay: 5000,
+  });
 
   return (
     <div className="hero-wrapper" id="home">
+      <animated.div
+        className="bg-overlay"
+        style={backgroundAnimation}
+      ></animated.div>
       <div className="hero-main">
         <h1>
-          Hi, I'm{" "}
-          <span
-            style={{
-              color: "#16f2b4",
-              textShadow: "3px 3px 20px #F433AB",
-              fontWeight: "bold",
-            }}
-          >
+          <animated.span style={hiFadeIn}>Hi.</animated.span>
+          <animated.span style={imFadeIn}> I'm </animated.span>
+          <animated.span style={nameFadeIn} className="jamalfox-header">
             Jamal Fox
-          </span>
+          </animated.span>
         </h1>
-        <div className="hero-profile-photo"></div>
-        <h1>Web Developer &#38; Designer</h1>
+        <animated.div
+          className="hero-profile-photo"
+          style={taglineFadeIn}
+        ></animated.div>
+        <animated.h1 style={taglineFadeIn}>Front-End Web Developer</animated.h1>
+        <animated.div style={particleFadeIn}>
+          <Particles
+            id="tsparticles"
+            options={{
+              background: {
+                color: {
+                  value: "transparent",
+                },
+              },
+              fpsLimit: 60,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#16f2b4",
+                },
+                links: {
+                  color: "transparent",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: false,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "out",
+                  random: false,
+                  speed: 2,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    value_area: 800,
+                  },
+                  value: 40,
+                },
+                opacity: {
+                  random: true,
+                },
+                shape: {
+                  type: ["circle"],
+                },
+                size: {
+                  random: true,
+                  value: 5,
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        </animated.div>
       </div>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "transparent",
-            },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#16f2b4",
-            },
-            links: {
-              color: "transparent",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: false,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "out",
-              random: false,
-              speed: 2,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 40,
-            },
-            opacity: {
-              random: true,
-            },
-            shape: {
-              type: ["circle", "star"],
-            },
-            size: {
-              random: true,
-              value: 5,
-            },
-          },
-          detectRetina: true,
-        }}
-      />
     </div>
   );
 };
--- src/Components/NAVBAR/Navbar.js
+++ src/Components/NAVBAR/Navbar.js
@@ -1,20 +1,42 @@
 import React from "react";
 import "./Navbar.css";
-import Testpdf from '../../Assets/Testpdf.pdf'
+import Testpdf from "../../Assets/Testpdf.pdf";
+import { useSpring, animated } from "react-spring";
 
 const Navbar = () => {
+  const navFadeIn = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 2500 },
+    delay: 5000,
+  });
+  const contactButtonSlideIn = useSpring({
+    to: {
+      right: "-10px",
+    },
+    from: {
+      right: "-100px",
+    },
+    config: { duration: 1000 },
+    delay: 7000,
+  });
+
   return (
     <div className="navbar-wrapper">
-      <div className="nav-links">
+      <animated.div className="nav-links" style={navFadeIn}>
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#skills">Skills</a>
         <a href="#portfolio">Portfolio</a>
-        <a href={Testpdf} target="_blank" rel="noreferrer">Resume</a>
-        <a href="#contact">
-          <button>Contact</button>
+        <a href={Testpdf} target="_blank" rel="noreferrer">
+          Resume
         </a>
-      </div>
+        <a href="#contact">
+          <animated.button style={contactButtonSlideIn}>
+            Contact
+          </animated.button>
+        </a>
+      </animated.div>
     </div>
   );
 };
