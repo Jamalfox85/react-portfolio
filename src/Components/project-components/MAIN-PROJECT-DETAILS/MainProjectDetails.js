9f696f6d9d5a6cabd4260e0575f58abe1fd7f5dd
Jamalfox85 <Fox.Jamal@outlook.com>
2021-11-21 20:31:32 -0500

    updated project details page. Data now loads from props and the page has the final grid template structure

--- src/Components/project-components/MAIN-PROJECT-DETAILS/MainProjectDetails.js
+++ src/Components/project-components/MAIN-PROJECT-DETAILS/MainProjectDetails.js
@@ -1,17 +1,51 @@
 import React from "react";
 import "./MainProjectDetails.css";
 
-const MainProjectDetails = () => {
+const MainProjectDetails = ({ desc, gitlink, title, image, tech }) => {
   return (
     <div className="mainprojectsdetails-wrapper">
       <div className="project-details-container">
-        <div className="project-details desc">Description</div>
-        <div className="project-details title">Project Title</div>
-        <div className="project-details git">Git</div>
-        <div className="project-details vid">Video</div>
-        <div className="project-details tech">Tech</div>
-        <div className="project-details testing">Testing</div>
-        <div className="project-details w3">w3</div>
+        <div className="project-details desc">
+          {desc}
+        </div>
+        <div className="project-details title">
+          <h1>{title}</h1>
+        </div>
+        <div className="project-details git">
+          <a href={gitlink} target="_blank" rel="noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+        <div className="project-details vid">
+          <img
+            src={image}
+            alt="project-thumbnail"
+            className="project-details-image"
+          />
+        </div>
+        <div className="project-details tech">
+          <h1>Technologies</h1>
+          <ul className="project-tech-list">
+            {tech.map((item, index) => (
+              <li>{tech[index]}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="project-details project-detail-buttons">
+          <a href={gitlink} target="_blank" rel="noreferrer">
+            <div className="project-detail-button pdb-view">VIEW</div>
+          </a>
+          <a href={gitlink} target="_blank" rel="noreferrer">
+            <div className="project-detail-button pdb-contact">CONTACT</div>
+          </a>
+          <a href={gitlink} target="_blank" rel="noreferrer">
+            <div className="project-detail-button pdb-back">BACK</div>
+          </a>
+          <a href={gitlink} target="_blank" rel="noreferrer">
+            <div className="project-detail-button pdb-back">BACK</div>
+          </a>
+        </div>
+        {/* <div className="project-details w3">w3</div> */}
       </div>
     </div>
   );
--- src/Pages/BruiserOutdoorsDetails.js
+++ src/Pages/BruiserOutdoorsDetails.js
@@ -1,10 +1,24 @@
 import React from "react";
 import MainProjectDetails from "../Components/project-components/MAIN-PROJECT-DETAILS/MainProjectDetails";
+import bruiserss from "../Assets/bruiserss.png";
+
+const description =
+  "This is the description of the project. This is the description of the product. This is the description. This is the description of the project. This is the description of the product. This is the description. This is the description of the project. This is the description of the product. This is the description. ";
+const gitlink = "https://github.com/Jamalfox85/BruiserOutdoors";
+const title = "Bruiser Outdoors";
+const screenshot = bruiserss
+const tech = ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'SQL', 'ASP.net']
 
 const Bruiseroutdoors_details = () => {
   return (
     <div className="details-wrapper">
-      <MainProjectDetails />
+      <MainProjectDetails
+        desc={description}
+        gitlink={gitlink}
+        title={title}
+        image={screenshot}
+        tech={tech}
+      />
     </div>
   );
 };
